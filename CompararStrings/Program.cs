0fc25b2fb235aa81edac032d5211ca43378c01cc
TiagoFSDC <tiagofsdc@gmail.com>
2023-03-08 08:54:01 -0300

    Add project files.

---
+++ CompararStrings/Program.cs
@@ -0,0 +1,15 @@
+﻿string texto, texto2;
+
+Console.WriteLine("Digite o primeiro texto");
+texto = Console.ReadLine();
+Console.WriteLine("Digite o segundo texto");
+texto2 = Console.ReadLine();
+
+if (texto == texto2)
+{
+    Console.WriteLine("Textos iguais");
+}
+else
+{
+    Console.WriteLine("Textos diferentes");
+}
